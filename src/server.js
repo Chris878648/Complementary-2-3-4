478636d8aa91a29a6819bc4df571f95a86e2ec81
Chris878648 <134880684+Chris878648@users.noreply.github.com>
2025-08-17 16:16:57 -0600

    Console Log new

--- src/server.js
+++ src/server.js
@@ -3,4 +3,6 @@ import app from "./app.js";
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
+  console.log('Server entry point');
 });
+
